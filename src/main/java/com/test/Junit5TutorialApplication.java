bb167f6bdfe6e5fe1c854a3f3f354714fc296c1f
wisdom08 <wisdom08@kakao.com>
2022-09-07 19:40:18 +0900

    Initial commit

---
+++ src/main/java/com/test/Junit5TutorialApplication.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Junit5TutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Junit5TutorialApplication.class, args);
+    }
+
+}
