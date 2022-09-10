9a4e7b38d6ccc48dae433569abcf40a55e0dd3ff
wisdom08 <wisdom08@kakao.com>
2022-09-11 08:46:44 +0900

    feat: dto에 validation 추가

--- src/main/java/com/test/web/dto/request/BookSaveReqDto.java
+++ src/main/java/com/test/web/dto/request/BookSaveReqDto.java
@@ -4,10 +4,19 @@ import com.test.domain.Book;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 public class BookSaveReqDto {
+
+    @Size(min = 1, max = 50)
+    @NotBlank
     private String title;
+
+    @Size(min = 2, max = 20)
+    @NotBlank
     private String author;
 
     public Book toEntity() {
